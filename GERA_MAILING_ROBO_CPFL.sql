1801167af4f89a9f31f5886055c02dbe1a2ed3be
João Victor Bezerra da Silva <100082917+JoaoVictorBezerra@users.noreply.github.com>
2022-06-03 14:13:16 -0300

    Add files via upload

--- GERA_MAILING_ROBO_CPFL.sql
+++ GERA_MAILING_ROBO_CPFL.sql
@@ -1,3 +1,5 @@
+USE easycollector
+/*============================================================== CRIAR VIEW TELEFONE_ORDEM ======================================================================*/
 CREATE VIEW TELEFONES_ORDEM  
 AS
 SELECT 
@@ -6,36 +8,35 @@ FROM TB_CLIENTE_TELEFONE
 WHERE TP_HABILITADO = 1 
 
 /*============================================================== MAILING PREVENTIVO DIA ======================================================================*/
-CREATE VIEW GERA_MAILING_ROBO_CPFL AS
-
+--DROP TRABLE ##ORDENA_MAILING
+--CREATE VIEW GERA_MAILING_ROBO_CPFL AS
 SELECT DISTINCT  
-'237' as CodigoCampanha,
-NU_CPF_CNPJ as CodigoCliente,
-'1' as TelefoneTipo,
-MAX(CASE 
-       WHEN A.ORDEM = 1 THEN CONVERT(varchar, A.NU_DDD)
-       ELSE ' '
-       END) AS DDD_01,
-MAX(CASE 
-       WHEN A.ORDEM = 1 THEN CONVERT(varchar, A.NU_TELEFONE)
-       ELSE ' '
-       END) AS TELEFONE_01,
-          '' as RamalAgendamento,
-          '' as DataAgendamento,
-  NM_NOME,
-  CONCAT(
-  NU_PRESTACAO,',',
-  VL_DIVIDA,',',
-  RIGHT(NM_CONTRATO_CEDENTE, 10),',',
-  B.ID_CLIENTE) AS [NUMERODEPARCELAS,VALORDIVIDA,NUMEROINSTALACAO,IDCON]
+	'237' as CodigoCampanha,
+	NU_CPF_CNPJ as CodigoCliente,
+	'1' as TelefoneTipo,
+	MAX(CASE 
+		   WHEN A.ORDEM = 1 THEN CONVERT(varchar, A.NU_DDD)
+		   ELSE ' '
+		   END) AS DDD_01,
+	MAX(CASE 
+		   WHEN A.ORDEM = 1 THEN CONVERT(varchar, A.NU_TELEFONE)
+		   ELSE ' '
+		   END) AS TELEFONE_01,
+	'' as RamalAgendamento,
+	'' as DataAgendamento,
+	NM_NOME,
+	CONCAT(
+		NU_PRESTACAO,',',
+		VL_DIVIDA,',',
+		RIGHT(NM_CONTRATO_CEDENTE, 10),',',
+		B.ID_CLIENTE) AS [NUMERODEPARCELAS,VALORDIVIDA,NUMEROINSTALACAO,IDCON]
 --INTO ##ORDENA_MAILING
 FROM TELEFONES_ORDEM A
-Join TB_CLIENTE B ON B.ID_CLIENTE = A.ID_CLIENTE
-join TB_CONTRATO ct on ct.ID_CLIENTE = B.ID_CLIENTE
-join TB_divida d on d.ID_contrato = ct.ID_Contrato
+	JOIN TB_CLIENTE B ON B.ID_CLIENTE = A.ID_CLIENTE
+	JOIN TB_CONTRATO CT ON CT.ID_CLIENTE = B.ID_CLIENTE
+	JOIN TB_divida D ON d.ID_contrato = CT.ID_Contrato
 WHERE ID_DEPOSITO ='86'
 GROUP BY NU_CPF_CNPJ, NM_NOME, NU_PRESTACAO, VL_DIVIDA, NM_CONTRATO_CEDENTE, B.ID_CLIENTE
 
 /*============================================================== Consulta o Mailing ======================================================================*/
-
 SELECT * FROM GERA_MAILING_ROBO_CPFL
