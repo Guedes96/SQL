3d5b70fe9543179860a49b0df1d1f46e756f753e
João Victor Bezerra da Silva <100082917+JoaoVictorBezerra@users.noreply.github.com>
2022-08-12 19:11:17 -0300

    Update Aula01.sql

--- Aula01.sql
+++ Aula01.sql
@@ -1,39 +1,41 @@
-
-
 CREATE DATABASE vendasJoao
 GO
-/*=================================================== CriaÁ„o e atribuiÁ„o de PK nas tabelas ===================================================*/
+USE vendasJoao
+GO
+/*=================================================== Cria√ß√£o das tabelas ===================================================*/
 IF OBJECT_ID('tbVenda','u') IS NOT NULL DROP TABLE tbVenda
 CREATE TABLE tbVenda (
 	  cdVendas INT NOT NULL
 	, dtVenda DATETIME NOT NULL
 	, vlTotal MONEY NOT NULL
-)
-GO
+);
+ 
 IF OBJECT_ID('tbVendaProduto','u') IS NOT NULL DROP TABLE tbVendaProduto
 CREATE TABLE tbVendaProduto (
 	  cdVenda INT NOT NULL
 	, cdProduto INT NOT NULL
 	, qtVendida INT
-)
-GO
+);
+
 IF OBJECT_ID('tbProduto','u') IS NOT NULL DROP TABLE tbProduto
 CREATE TABLE tbProduto (
 	  cdProduto INT NOT NULL
 	, dsProduto VARCHAR(40) NOT NULL
 	, vlProduto MONEY
-)
-GO
+);
+
+/*=================================================== Atribui√ß√£o das chaves prim√°rias ===================================================*/
+
 ALTER TABLE tbVenda
-	ADD PRIMARY KEY (cdVenda)
-GO
+	ADD PRIMARY KEY (cdVendas)
+
 ALTER TABLE tbVendaProduto
 	  ADD FOREIGN KEY (cdVenda)
 	  REFERENCES tbVenda
-GO
+
 ALTER TABLE tbVendaProduto
 	  ADD FOREIGN KEY (cdProduto)
 	  REFERENCES tbProduto
-GO
+
 ALTER TABLE tbProduto
 	ADD PRIMARY KEY (cdProduto)
